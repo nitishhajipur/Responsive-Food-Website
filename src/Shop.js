1b2070e9eee26ff1757ba5789e26741ff5151e81
Nitish <nitishhajipur01@gmail.com>
2022-01-14 16:28:59 +0530

    test

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Withdraw or Deposit money</h2>
+            <button type="button" class="btn btn-primary me-1">-</button>
+             Update Balance
+            <button type="button" class="btn btn-primary ms-1">+</button>
+        </div>
+    )
+}
+
+export default Shop
---
+++ src/state/action-creator/index.js
@@ -0,0 +1,16 @@
+export const withdrawMoney = (amount) =>{
+    return (dispatch) => {
+        dispatch({
+      type: 'withdraw',
+      payload: amount})
+    }
+}
+
+export const depositMoney = (amount) =>{
+    return (dispatch) =>{
+        dispatch({
+            type:'deposit',
+            payload: amount
+        })
+    }
+}
